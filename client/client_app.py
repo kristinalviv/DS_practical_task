346f4b3684505501574bbd6a611c3ef93babb51a
Khrystyna <klepykh@gmail.com>
2023-07-24 22:07:04 +0300

    fixed first task

--- client/client_app.py
+++ client/client_app.py
@@ -1,7 +1,5 @@
 import socket
-import json
 import logging
-import itertools
 from datetime import datetime, timedelta
 from inputimeout import inputimeout
 
@@ -14,15 +12,14 @@ class Client:
 		self.port = port
 
 	def __str__(self):
-		return f'Client connects to  {self.host}:{self.port} contain following messages: {Client.cl_msg_lst}.'
+		return f'Client connected to  {self.host} contain following messages: {Client.cl_msg_lst}.'
 
 	def __repr__(self):
 		return f'Client({self.host}:{self.port})'
 
-	cl_msg_id = itertools.count(1)
+	cl_msg_id = 0
+	cl_msgs = set()
 	cl_msg_lst = {}
-	cl_msg_lst_final = {}
-	cl_msg_id_final = itertools.count(1)
 
 	def create_connection(self, host, port):
 		try:
@@ -43,44 +40,18 @@ class Client:
 						print(client.get_messages())
 				except Exception as e:
 					print(e)
-				max_message_time = datetime.now() + timedelta(hours=0, minutes=2, seconds=00)
-				print(max_message_time)
 				server_message = client_socket.recv(1024).decode()
-				message_time = datetime.now()
-				print(message_time)
+				max_message_time = datetime.now() + timedelta(hours=0, minutes=0, seconds=20)
 				if not server_message:
 					logging.info('No message from the server side...')
 					break
-				elif message_time > max_message_time:
-					logging.info('Received old message(-s). Skipping it...')
-					continue
 				print(f'Received from the server - {server_message}')
-				cl_message_id = next(Client.cl_msg_id)
-				Client.cl_msg_lst.update({cl_message_id: f'{server_message}'})
-				print('Successfully pre-saved message.')
-				logging.info(f'Client approved message! ID is: {cl_message_id}')
-				client_socket.send(f'{cl_message_id}'.encode())
-				try:
-					socket.setdefaulttimeout(20)
-					final_approval = client_socket.recv(1024).decode()
-					if final_approval == 'Approved':
-						print(f'Received {final_approval} message from the server.')
-						final_cl_message_id = next(Client.cl_msg_id_final)
-						Client.cl_msg_lst_final.update({final_cl_message_id: f'{server_message}'})
-						print(f'Message successfully saved')
-						socket.setdefaulttimeout(None)
-					else:
-						print('Turned message ID back since unsaved.')
-						count = Client.cl_msg_id.__reduce__()[1][0] - 1
-						Client.cl_msg_id = itertools.count(count)
-						socket.setdefaulttimeout(None)
-				except socket.timeout as e:
-					print('Turned message ID back since unsaved.')
-					count = Client.cl_msg_id.__reduce__()[1][0] - 1
-					Client.cl_msg_id = itertools.count(count)
-					socket.setdefaulttimeout(None)
-
-
+				split_message = server_message.split('-')
+				message_id = split_message[0]
+				message = split_message[1:]
+				self.cl_msg_lst.update({message_id: f"{message}"})
+				client_socket.send('Replicated'.encode())
+				print(f'Message successfully saved')
 		except Exception as e:
 			client_socket.close()
 			print('Connection closed')
@@ -91,9 +62,8 @@ class Client:
 		client_socket.close()
 		print('Connection closed')
 
-	@staticmethod
-	def get_messages():
-		return Client.cl_msg_lst_final
+	def get_messages(self):
+		return self.cl_msg_lst
 
 
 if __name__ == "__main__":
@@ -101,5 +71,5 @@ if __name__ == "__main__":
 	print(client)
 	client_socket = client.create_connection(client.host, client.port)
 	client.client_app(client_socket)
-	print(client.get_messages())
+	print(client.__str__())
 	client.close_connection(client_socket)
--- server/server_app.py
+++ server/server_app.py
@@ -1,6 +1,6 @@
 import socket
-import itertools
 import logging
+from datetime import datetime
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s--%(levelname)s--%(message)s')
 
@@ -15,12 +15,15 @@ class ServerApp:
 	def __repr__(self):
 		return f'ServerApp({self.host}, {ServerApp.msg_lst})'
 
-	msg_id = itertools.count(1)
+	def __aiter__(self, co):
+		return self
+
+	msg_id = 0
 	msg_lst = {}
 
 	def create_server_socket(self, port=4040):
 		try:
-			socket.setdefaulttimeout(20)  # set 20 seconds timeout
+			socket.setdefaulttimeout(20)
 			server_socket = socket.socket()
 			server_socket.bind((self.host, port))
 			print(f'Server connection is open on {self.host} with {port} port.')
@@ -33,6 +36,7 @@ class ServerApp:
 		try:
 			connections = []
 			server_socket.listen(listen_counts)
+			print(socket.getdefaulttimeout())
 			while listen_counts > 0:
 				conn, address = server_socket.accept()
 				listen_counts -= 1
@@ -46,89 +50,54 @@ class ServerApp:
 			for unique_conn in connections:
 				unique_conn.close()
 
-	# def message_processing(self, connections, message, answer_count=0):
-	# 	for number, unique_conn in enumerate(connections, start=1):
-	# 		unique_conn.send(f'{message}'.encode())
-	# 		logging.info(f'Successfully sent to {number} client')
-	# 		id_received = unique_conn.recv(1024).decode()
-	# 		logging.info(f'Received ID from {number} node is {id_received}')
-	# 		answer_count += 1
-	# 	return answer_count
-
-	def message_approval(self, connections, message, write_concern, max_retry):
-		answer_count = 0
-		retry = 0
-		# for unique_conn in connections:
-		# 	unique_conn.send(f'{message}'.encode())
-		logging.info(f'Write concern is {write_concern}.')
-		logging.info(f'Starting communication with client nodes...')
-		while (retry <= max_retry) & (answer_count < write_concern):
-			try:
-				# answer_count = ServerApp().message_processing(connections, message)
-				for number, unique_conn in enumerate(connections, start=1):
-					unique_conn.send(f'{message}'.encode())
-					logging.info(f'Successfully sent to {number} client')
-					id_received = unique_conn.recv(1024).decode()
-					logging.info(f'Received ID from {number} node is {id_received}')
-					answer_count += 1
-			except socket.timeout as e:
-				logging.info(e)
-				logging.info(f'Did not save this message. Timeout occurs!')
-				retry += 1
-				answer_count=0
-				print(f'Starting {retry} retry.')
-				print(f'Max retry is: {max_retry}')
-			except Exception as e:
-				logging.info(e)
-		logging.info(f'Finished, received answer(s) is (are) {answer_count}.')
-		return answer_count
-
-	def proceed_message(self, server_socket, connections, write_concern=3, max_retry=2):
+	def proceed_message(self, server_socket, connections):
+		write_concern = 3
 		while True:
 			try:
 				message = input('Please enter your message here...:)')
 				if message in ['exit', 'end', 'quit', 'q']:
 					break
 				else:
-					logging.info(f'Your message is - {message}')
+					self.msg_id += 1
+					message_id = self.msg_id
+					success_repl = 0
+					logging.info(f'Your message is - {message_id} - {message}')
 					logging.info('Sending message to the client...')
-					# for unique_conn in connections:
-					# 	unique_conn.send(f'{message}'.encode())
-					# logging.info('Successfully sent to clients...')
-					# logging.info(f'Write concern is {write_concern}.')
-					# logging.info(f'Starting receiving answer from client nodes...')
-					# maybe it should be method
-					# for number, unique_conn in enumerate(connections, start=1):
-					# 	try:
-					# 		id_received = unique_conn.recv(1024).decode()
-					# 		logging.info(f'Received ID from {number} node is {id_received}')
-					# 		answer_count += 1
-					# 	except socket.timeout as e:
-					# 		logging.info(e)
-					# 		logging.info(f'Did not save this message. Timeout occurs!')
-					# 	except Exception as e:
-					# 		logging.info(e)
-					# logging.info(f'Finished, received answer(s) is (are) {answer_count}.')
-					# maybe it should be method
-					answer_count = ServerApp().message_approval(connections, message, write_concern, max_retry)
-					if answer_count >= write_concern:
-						logging.info('Write concern fulfilled. ')
-						ServerApp.msg_lst.update({next(ServerApp.msg_id): f'{message}'})
+					try:
 						for unique_conn in connections:
-							unique_conn.send(f'Approved'.encode())
-						logging.info('Successfully sent approval to clients...')
-					else:
-						logging.info('Write concern was NOT fulfilled. Message was not saved')
+							unique_conn.send(f'{message_id}-{message}'.encode())
+						logging.info('Successfully sent to clients...')
+						for number, unique_conn in enumerate(connections, start=1):
+							response = unique_conn.recv(1024).decode()
+							logging.info(f'Received from {number} node response: {response}')
+							if response == 'Replicated':
+								success_repl += 1
+								print(f'Successfully replicated.')
+							else:
+								logging.info(f'Replication error, received following response:{response}.')
+						if success_repl == write_concern:
+							logging.info('Write concern fulfilled - date=%s', datetime.now())
+							self.msg_lst.update({message_id: f"{message}"})
+							logging.info(f'Replication to {success_repl} nodes was performed successfully.')
+						elif success_repl < write_concern:
+							logging.info(f"Write concern was NOT fulfilled.")
+							self.msg_id -= 1
+						logging.info(f'Write concern is {write_concern}.')
+					except socket.timeout as e:
+						message_id -= 1
+						logging.info(e)
+						logging.info(f'Did not save this message. Timeout occurs!')
+					except Exception as e:
+						message_id -= 1
+						logging.info(f'Did not save this message. {e}')
 			except Exception as e:
+				message_id -= 1
 				server_socket.close()
 				for unique_conn in connections:
 					unique_conn.close()
 				print('Connection closed')
 				print(f'Could not save your message, {e}')
 
-	@staticmethod
-	def get_messages():
-		return ServerApp.msg_lst
 
 
 if __name__ == "__main__":
@@ -136,7 +105,7 @@ if __name__ == "__main__":
 	server_socket = server.create_server_socket()
 	connections, address, listen_counts = server.connect_to_replicas(server_socket)
 	server.proceed_message(server_socket, connections)
-	print(ServerApp.get_messages())
+	print(server.__str__())
 	server_socket.close()
 	for unique_conn in connections:
 		unique_conn.close()
